979372d27074aa62762f81f6acca4bb8593231b2
unknown <Schavezmoraga@gmail.com>
2020-07-28 00:39:41 -0600

    Segunda_parte_de_mi_credo

---
+++ Credo.io/Scripts/js.js
@@ -0,0 +1,22 @@
+!(function($) {
+$(".mentores-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
+})(jQuery);
+  $("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
